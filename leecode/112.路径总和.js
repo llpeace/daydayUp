727d35d4088d84e5a7c91b807448283d81d8dd3e
llpeace <1633201235@qq.com>
2021-08-24 22:07:06 +0800

    feat: 2021/08/24

---
+++ leecode/112.路径总和.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=javascript
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    const find = (node, val) => {
+        if (!node) return false;
+        if (val + node.val === targetSum && !node.left && !node.right) return true;
+        return find(node.left, val + node.val) || find(node.right, val + node.val);
+    }
+
+    return find(root, 0);
+};
+// @lc code=end
+
