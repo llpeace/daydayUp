5b01afbc058613ddbda8a3ad31a85899e6143657
llpeace <1633201235@qq.com>
2022-03-23 23:36:35 +0800

    feat: 2022/03/23

---
+++ leecode/143.重排链表.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=143 lang=javascript
+ *
+ * [143] 重排链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    let newHead = new ListNode();
+    let x = head;
+    while(x) {
+        1
+        x = x.next;
+    }
+};
+// @lc code=end
+
